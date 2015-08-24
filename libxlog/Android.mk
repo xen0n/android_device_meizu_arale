34864f39634951c00cae982a9535dac336f1ced7
Wang Xuerui <idontknw.wang@gmail.com>
2015-08-25 02:48:32 +0800

    remove mtkaudio from libxlog
    Change-Id: Ide44ea448bfce255f217489c1bd1f95c60cca594

--- libxlog/Android.mk
+++ libxlog/Android.mk
@@ -19,8 +19,8 @@ include $(CLEAR_VARS)
 
 LOCAL_CFLAGS += $(LIBLOG_CFLAGS)
 LOCAL_MODULE := libxlog
-LOCAL_SRC_FILES := xlog.c mtkaudio.cpp
-LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
-LOCAL_SHARED_LIBRARIES := libcutils liblog libutils libbinder
+LOCAL_SRC_FILES := xlog.c
+LOCAL_C_INCLUDES += system/core/include/
+LOCAL_SHARED_LIBRARIES := libcutils liblog
 
 include $(BUILD_SHARED_LIBRARY)
