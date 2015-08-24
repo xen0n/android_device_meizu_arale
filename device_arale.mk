311d02b33ef3c7b1211ef3d2dc26b91b9b0687c4
Wang Xuerui <idontknw.wang@gmail.com>
2015-08-24 11:58:16 +0800

    fix typo
    Change-Id: Ia8ca62dcd156b1f3b75a58e21e4bc04be629f77e

--- device_arale.mk
+++ device_arale.mk
@@ -4,7 +4,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/meizuz/arale/arale-vendor.mk)
+$(call inherit-product-if-exists, vendor/meizu/arale/arale-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/meizu/arale/overlay
 
