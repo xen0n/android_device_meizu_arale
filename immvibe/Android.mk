c16e10833baa466a152fa4e9db47d0ec32034582
Wang Xuerui <idontknw.wang@gmail.com>
2015-09-28 15:17:47 +0800

    immvibe: shorter file names
    Change-Id: Iecf62d0c6fa882df9cf16fc828724e850f51dedc

--- immvibe/Android.mk
+++ immvibe/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE                  := libimmvibeclient
 LOCAL_MODULE_TAGS             := optional
-LOCAL_SRC_FILES               := immvibeclient.c
+LOCAL_SRC_FILES               := client.c
 LOCAL_EXPORT_C_INCLUDES       := $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES              := $(LOCAL_EXPORT_C_INCLUDES)
 include $(BUILD_SHARED_LIBRARY)
@@ -11,7 +11,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE                  := immvibetst
 LOCAL_MODULE_TAGS             := optional
-LOCAL_SRC_FILES               := immvibetst.c
+LOCAL_SRC_FILES               := tst.c
 
 # hack to suppress build errors when building for the first time (?)
 LOCAL_C_INCLUDES              := $(LOCAL_PATH)/include
