9ae5dbfebb2d587d92e746950d7ba497a6bd81d9
Wang Xuerui <idontknw.wang@gmail.com>
2015-10-15 21:50:58 +0800

    boot: remove MTK boot.img hack (and add a couple of smaller ones)
    This depends on xen0n/android_system_core_mtk@1c9eeb9.
    
    Change-Id: Icf8cf965e4a1ec15927d063eb0ffea3a0b87c4a2

--- boot.mk
+++ boot.mk
@@ -14,48 +14,13 @@
 # limitations under the License.
 #
 
-# can this be done in bash? credits go to bgcngm
-define make_header
-  perl -e 'print pack("a4 L a32 a472", "\x88\x16\x88\x58", $$ARGV[0], $$ARGV[1], "\xFF"x472)' $(1) $(2) > $(3)
-endef
-
-# this is overriding targets from build/core/Makefile
-$(INSTALLED_KERNEL_TARGET).mtk.header: $(INSTALLED_KERNEL_TARGET)
-	size=$$($(call get-file-size,$(INSTALLED_KERNEL_TARGET))); \
-		$(call make_header, $$((size)), "KERNEL", $@)
-$(INSTALLED_KERNEL_TARGET).mtk: $(INSTALLED_KERNEL_TARGET).mtk.header
-	$(call pretty,"Adding MTK header to kernel.")
-	cat $(INSTALLED_KERNEL_TARGET).mtk.header $(INSTALLED_KERNEL_TARGET) \
-		> $@
-
-$(INSTALLED_RAMDISK_TARGET).mtk.header: $(INSTALLED_RAMDISK_TARGET)
-	size=$$($(call get-file-size,$(INSTALLED_RAMDISK_TARGET))); \
-		$(call make_header, $$((size)), "ROOTFS", $@)
-$(INSTALLED_RAMDISK_TARGET).mtk: $(INSTALLED_RAMDISK_TARGET).mtk.header
-	$(call pretty,"Adding MTK header to ramdisk.")
-	cat $(INSTALLED_RAMDISK_TARGET).mtk.header $(INSTALLED_RAMDISK_TARGET) \
-		> $@
-
-$(PRODUCT_OUT)/recovery_kernel.mtk.header: $(recovery_kernel)
-	size=$$($(call get-file-size,$(recovery_kernel))); \
-		$(call make_header, $$((size)), "KERNEL", $@)
-$(PRODUCT_OUT)/recovery_kernel.mtk: $(PRODUCT_OUT)/recovery_kernel.mtk.header
-	$(call pretty,"Adding MTK header to recovery kernel.")
-	cat $(PRODUCT_OUT)/recovery_kernel.mtk.header $(recovery_kernel) > $@
-
-$(recovery_ramdisk).mtk.header: $(recovery_ramdisk)
-	size=$$($(call get-file-size,$(recovery_ramdisk))); \
-		$(call make_header, $$((size)), "RECOVERY", $@)
-$(recovery_ramdisk).mtk:  $(MKBOOTIMG) $(recovery_ramdisk).mtk.header
-	$(call pretty,"Adding MTK header to recovery ramdisk.")
-	cat $(recovery_ramdisk).mtk.header $(recovery_ramdisk) > $@
-
 INTERNAL_MTK_BOOTIMAGE_ARGS := \
-		--kernel $(INSTALLED_KERNEL_TARGET).mtk \
-		--ramdisk $(INSTALLED_RAMDISK_TARGET).mtk
+		--kernel $(INSTALLED_KERNEL_TARGET) \
+		--ramdisk $(INSTALLED_RAMDISK_TARGET) \
+		--mtk boot
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG)\
-		$(INSTALLED_RAMDISK_TARGET).mtk $(INSTALLED_KERNEL_TARGET).mtk
+		$(INSTALLED_RAMDISK_TARGET) $(INSTALLED_KERNEL_TARGET)
 	$(call pretty,"Target boot image: $@")
 	$(MKBOOTIMG) $(INTERNAL_MTK_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) \
 		--output $@
@@ -64,11 +29,12 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG)\
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
 INTERNAL_MTK_RECOVERYIMAGE_ARGS := \
-		--kernel $(PRODUCT_OUT)/recovery_kernel.mtk \
-		--ramdisk $(recovery_ramdisk).mtk
+		--kernel $(recovery_kernel) \
+		--ramdisk $(recovery_ramdisk) \
+		--mtk recovery
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-		$(recovery_ramdisk).mtk $(PRODUCT_OUT)/recovery_kernel.mtk
+		$(recovery_ramdisk) $(recovery_kernel)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
 	$(MKBOOTIMG) $(INTERNAL_MTK_RECOVERYIMAGE_ARGS) \
 		$(BOARD_MKBOOTIMG_ARGS) --output $@
