ee319a7b8d5b5822920216f43e638e71472c54af
Wang Xuerui <idontknw.wang@gmail.com>
2016-03-25 15:45:21 +0800

    device_common: seems PRODUCT_COPY_FILES doesn't like target variables
    Change-Id: Ifd5088711437fc85946c73a0cd0683530b605d23

--- device_common.mk
+++ device_common.mk
@@ -152,11 +152,11 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/root/enableswap.sh:root/enableswap.sh \
-    $(LOCAL_PATH)/root/fstab.$(TARGET_BOARD_PLATFORM):root/fstab.$(TARGET_BOARD_PLATFORM) \
+    $(LOCAL_PATH)/root/fstab.mt6595:root/fstab.mt6595 \
     $(LOCAL_PATH)/root/twrp.fstab:recovery/root/etc/twrp.fstab \
-    $(LOCAL_PATH)/root/init.$(TARGET_BOARD_PLATFORM).rc:root/init.$(TARGET_BOARD_PLATFORM).rc \
-    $(LOCAL_PATH)/root/init.$(TARGET_BOARD_PLATFORM).usb.rc:root/init.$(TARGET_BOARD_PLATFORM).usb.rc \
-    $(LOCAL_PATH)/root/init.recovery.$(TARGET_BOARD_PLATFORM).rc:root/init.recovery.$(TARGET_BOARD_PLATFORM).rc \
+    $(LOCAL_PATH)/root/init.mt6595.rc:root/init.mt6595.rc \
+    $(LOCAL_PATH)/root/init.mt6595.usb.rc:root/init.mt6595.usb.rc \
+    $(LOCAL_PATH)/root/init.recovery.mt6595.rc:root/init.recovery.mt6595.rc \
     $(LOCAL_PATH)/root/init.aee.rc:root/init.aee.rc \
     $(LOCAL_PATH)/root/init.modem.rc:root/init.modem.rc \
     $(LOCAL_PATH)/root/init.volte.rc:root/init.volte.rc \
@@ -166,7 +166,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/root/init.project.rc:root/init.project.rc \
     $(LOCAL_PATH)/root/init.ssd.rc:root/init.ssd.rc \
     $(LOCAL_PATH)/root/init.xlog.rc:root/init.xlog.rc \
-    $(LOCAL_PATH)/root/ueventd.$(TARGET_BOARD_PLATFORM).rc:root/ueventd.$(TARGET_BOARD_PLATFORM).rc \
+    $(LOCAL_PATH)/root/ueventd.mt6595.rc:root/ueventd.mt6595.rc \
     $(LOCAL_PATH)/root/sbin/busybox:root/sbin/busybox \
     $(LOCAL_KERNEL):kernel
 
